b7f37ee3b49290b3d3c2733f615aae28f91329a4
dvory <dvoryannikova.alina@gmail.com>
2024-12-05 15:36:23 +0200

    fix build

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,4 +1,11 @@
-import SubmittedData from "@/features/SubmittedData";
+"use client";
+
+import dynamic from "next/dynamic";
+
+// for localstorage usage
+const SubmittedData = dynamic(() => import("@/features/SubmittedData"), {
+  ssr: false,
+});
 
 export default function ThankYouPage() {
   return <SubmittedData />;
